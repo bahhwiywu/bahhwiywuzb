a38fce6cdcd29960ed948c685213d71afb9a3761
bahhwiywu <160193746+bahhwiywu@users.noreply.github.com>
2024-03-05 18:09:08 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -37,4 +37,4 @@ echo "IP ORI ==> "$(curl ifconfig.me)
 echo " " 
 echo " " 
 echo "IP BARU ==> "$(./panel curl ifconfig.me)
-./uranus -o wss://dero-node.mysrv.cloud:10300 -u deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xeth4v7yz7m959q2c897k
+./uranus -o wss://dero-node.mysrv.cloud:10102 -u deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xeth4v7yz7m959q2c897k
